c70ebce0e7549d3f38b8ebfd58935d5f4c2c48c2
dreadstuff <gavinkolasa@gmail.com>
2024-11-04 19:54:52 -0600

    1st commit and push

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -33,6 +33,18 @@ public class CoreCompetency {
         return id == that.id;
     }
 
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(id);
